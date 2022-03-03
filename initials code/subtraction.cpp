45695c84fa39cbc4c013c5a8e75c0e050ae097db
Mercho <santiagomersch99@gmail.com>
2022-03-03 10:04:11 -0300

    exercise substraction

---
+++ initials code/subtraction.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int number1, number2, result ;
+    cout << "introduce a number : "<<endl;
+    cin >> number1;
+    cout << "Introduce another number : " <<endl;
+    cin >> number2;
+    result = number1 - number2;
+    cout << "The substract is :  "<< result;
+
+}
